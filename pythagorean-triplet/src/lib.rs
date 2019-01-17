b3f3709ff49a188d004c3ab048e4aef576f7f2aa
Joaquín Gumucio <jjgumucio@gmail.com>
2019-01-17 16:24:48 -0300

    Solves Pythagorean-Triplet

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,15 +1,55 @@
+// Struct to generate numbers
+#[derive(Copy, Clone)]
+struct Generator {
+    number: u32,
+    limit: u32,
+}
+
+impl Generator {
+    fn new(limit: u32) -> Generator {
+        Generator {
+            number: 1,
+            limit
+        }
+    }
+}
+
+impl Iterator for Generator {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        self.number += 1;
+
+        if self.number < self.limit {
+            Some(self.number)
+        } else {
+            None
+        }
+    }
+}
+
 pub fn find() -> Option<u32> {
     let mut a: u32 = 0;
     let mut b: u32 = 0;
     let mut c: u32 = 0;
-    for m in 1..500 {
-        let n: u32 = m as u32 + 1;
-        a = n.pow(2) - (m as u32).pow(2);
-        b = 2 * n * m;
-        c = n.pow(2) + m.pow(2);
-        if a + b + c == 1000 {
-            println!("FOUND CANDIDATES: a: {}, b: {}, c: {}", a, b, c);
+
+    let generator_a = Generator::new(1000);
+    let generator_b = Generator::new(1000);
+    let generator_c = Generator::new(1000);
+
+    for x in generator_a {
+        for y in generator_b {
+            for z in generator_c {
+                if x + y + z == 1000 {
+                    if x.pow(2) + y.pow(2) == z.pow(2) {
+                        a = x;
+                        b = y;
+                        c = z;
+                        break;
+                    }
+                }
+            }
         }
     }
-    Some(a + b + c)
+    Some(a * b * c)
 }
